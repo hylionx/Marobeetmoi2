7cf1a0a4fb313bf409a7dddae27096ea45757561
JUGURTHA <jugurtha.1703@gmail.com>
2020-06-27 16:17:45 +0200

    sup bdd, gitignore, indentation

--- class/Articles.php
+++ class/Articles.php
@@ -7,9 +7,5 @@ class Articles{
         $req = Database::connect()->query("SELECT id,nom, prix FROM robes ORDER BY prix");
         return $req->fetchAll();
     }
-
-
-
-
 }
 ?>
--- class/Database.php
+++ class/Database.php
@@ -1,20 +1,17 @@
 <?php
+    define('HOST','localhost');
+    define('DB_NAME','marobeetmoi2');
+    define('PORT','3308');
+    define('USER', 'root');
+    define('PASS','');
 
-define('HOST','localhost');
-define('DB_NAME','marobeetmoi2');
-define('USER', 'root');
-define('PASS','');
+    try { 
 
-try { $db=new PDO("mysql:host=".HOST.";dbname=".DB_NAME,USER,PASS);
-        $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-}catch(PDOException $e){
-    echo $e;
-
-}
- 
-
-  
+        $db=new PDO("mysql:host=".HOST.";dbname=".DB_NAME.";port=".PORT,USER,PASS,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     
+    } catch(PDOException $e) {
 
+        echo $e;
+
+    }
 ?>
